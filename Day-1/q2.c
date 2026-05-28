f32d77ff7950db2a925007360550152a8a38246d
Shivam Saraswat <216927038+heysaraswat@users.noreply.github.com>
2026-05-28 22:47:38 +0530

    Implement multiplication table program

---
+++ Day-1/q2.c
@@ -0,0 +1,16 @@
+// program to Print multiplication table of a given number.
+
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter your number = ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= 10; i++)
+    {
+        printf("%d * %d => %d\n", n, i, (n * i));
+    }
+
+    return 0;
+}
