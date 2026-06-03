9b2a2226632e9084f4b243bcebc15bb7129380b7
Shivam Saraswat <216927038+heysaraswat@users.noreply.github.com>
2026-06-04 00:01:29 +0530

    Implement LCM of 2 Number Program

---
+++ Day-3/q12.cpp
@@ -0,0 +1,29 @@
+// Write a program to Find LCM of two numbers.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n1, n2;
+
+    cout << "Enter number 1 = ";
+    cin >> n1;
+
+    cout << "Enter number 2 = ";
+    cin >> n2;
+
+    int lcm;
+
+    for (int i = 1; i < max(n1, n2); i++)
+    {
+        if (i % n1 == 0 && i % n2 == 0)
+        {
+            lcm = i;
+            break;
+        }
+    }
+
+    cout << "LCM of " << n1 << " and " << n2 << " => " << lcm << endl;
+
+    return 0;
+}
